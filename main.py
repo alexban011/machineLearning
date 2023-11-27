162be90155d7d66e9d5bc32f5b5825159210512d
alexban011 <alexban011@gmail.com>
2023-11-27 12:26:56 +0200

    lab2

--- main.py
+++ main.py
@@ -7,60 +7,54 @@ import pandas as pd
 
 def k_means(points, k, tol, max_iter):
     centroids = []
+    random_points = []
+    currIter = 0
 
-    # Randomly initialize centroids
-    random_points = random.sample(list(points), k)
+    for i in range(k):
+        random_points.append([random.uniform(-300, 300),random.uniform(-300, 300)])
+
     for point in random_points:
         centroids.append(point)
 
     for i in range(max_iter):
+        currIter = i
         classes = {}
         for j in range(k):
             classes[j] = []
 
-        # Assign points to nearest centroid
         for point in points:
             distances = [np.linalg.norm(point - centroid) for centroid in centroids]
             nearest_centroid = distances.index(min(distances))
             classes[nearest_centroid].append(point)
 
-        # Save old centroids for convergence check
         old_centroids = centroids.copy()
 
-        # Calculate new centroids
         for j in range(k):
             centroids[j] = np.average(classes[j], axis=0)
 
-        # Plot points and Centroid
-        colors = 10 * ['r', 'g', 'b', 'c', 'k', 'y', 'm']
+        colors = 10 * ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
         for j in range(k):
             color = colors[j]
             features = np.array(classes[j])
             plt.scatter(features[:, 0], features[:, 1], color=color, s=1)
-            plt.scatter(centroids[j][0], centroids[j][1], s=300, c="black")
-
-        plt.show()
-        if i < max_iter - 1:
-            plt.pause(1)  # Pause for 2 seconds
-            plt.clf()  # Clear the figure for the next plot
+            plt.scatter(centroids[j][0], centroids[j][1], s=100, c="black")
 
         plt.show()
 
-        # Check if we have converged
         isOptimal = True
-
         for j in range(k):
             original_centroid = old_centroids[j]
             current_centroid = centroids[j]
-
-            if np.sum((current_centroid - original_centroid) / original_centroid * 100.0) > tol:
+            if np.sum((current_centroid - original_centroid)) > tol:
                 isOptimal = False
 
-                # If centroids don't change significantly, we can stop
         if isOptimal:
-            break
+            return currIter
 
-    return centroids, classes
+        if i < max_iter - 1:
+            plt.pause(1)
+            plt.clf()
+        plt.show()
 
 
 def main():
@@ -69,21 +63,13 @@ def main():
     df = df.dropna()
     points = df[[0, 1]].to_numpy()
 
-    k = random.randint(2, 10)  # Define number of clusters
-    tol = 0.001  # Define tolerance
-    max_iter = 10  # Define maximum iterations
+    k = random.randint(2, 10)
+    tol = 2
+    max_iter = 100
 
     print(f"Centroids: ${k}")
 
-    centroids, classes = k_means(points, k, tol, max_iter)  # Run k-means
-
-    cost = 0
-    for centroid_index in classes:
-        for point in classes[centroid_index]:
-            cost += np.linalg.norm(centroids[centroid_index] - point)
-
-    print('Cost of the model:', cost)
-
+    print(f"Iterations: ${k_means(points, k, tol, max_iter)}")
 
 if __name__ == "__main__":
     main()
